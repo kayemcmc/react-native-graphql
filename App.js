d018562976126f23532c3c812d3c739a2a8299a5
Karen Posada <k100posada@gmail.com>
2018-05-05 13:42:01 -0500

    adding createStackNavigator, post component, two page nav

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import navStyles from './styles/navStyles';
 
-export default class App extends React.Component {
+import Post from './Post';
+
+class App extends React.Component {
+  static navigationOptions = {
+    title: "Home",
+    ...navStyles  
+  };
+
+  goToPost = () => {
+    this.props.navigation.navigate('Post');
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello Alessandro</Text>
+        <Button 
+          onPress={this.goToPost}
+          title="Go to Post page"
+        />
       </View>
     );
   }
@@ -20,3 +35,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default createStackNavigator({
+  Home: {
+    screen: App
+  },
+  Post: {
+    screen: Post
+  }
+})
